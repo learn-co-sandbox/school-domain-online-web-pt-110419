cdd87ffb0b449e0d9eceb4b455144924c51e8351
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-26 01:01:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,12 @@ class School
   end 
   
   def add_student(name, grade)
- roster[grade] = [name]
- end 
+	  if roster.has_key?(grade
+	    roster[grade] << name
+	  else
+	    roster[grade] = [name]
+	  end 
+	end 
  
  
 end 
